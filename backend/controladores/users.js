523fea05da10d7a7d3916c2d10c663836b297c41
Daniel Teruggi <sdteruggi@gmail.com>
2024-09-23 17:22:06 -0300

    version5

--- backend/controladores/users.js
+++ backend/controladores/users.js
@@ -1,8 +1,12 @@
 se agrega la nueva base de datos 
 para login de UserActivation
 
+<<<<<<< HEAD
 la nueva version 2.0
 
 modification numbre 3.0
 
 actualizacion 4.0
+=======
+la nueva version
+>>>>>>> parent of 4439ec5 (version3 bkend-user)
